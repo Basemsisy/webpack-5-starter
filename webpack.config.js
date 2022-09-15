941edb7a7a721fb0207f3f7fa8014b75989cc731
Basem Elsisy <basemsisy@gmail.com>
2022-09-15 21:51:04 +0200

    feat: install html plugin & scc, sass loader and use them

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,6 @@
 const path = require("path");
+const htmlWebpackPlugin = require("html-webpack-plugin");
+
 module.exports = {
   mode: "production",
   entry: path.resolve(__dirname, "src/index.js"),
@@ -6,4 +8,18 @@ module.exports = {
     path: path.resolve(__dirname, "dist"),
     filename: "main.js",
   },
+  module: {
+    rules: [
+      {
+        test: /\.scss$/,
+        use: ["style-loader", "css-loader", "sass-loader"],
+      },
+    ],
+  },
+  plugins: [
+    new htmlWebpackPlugin({
+      title: "Learn Webpack",
+      filename: "./index.html",
+    }),
+  ],
 };
