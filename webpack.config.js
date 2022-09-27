19551c943532eb0939d566567e0ff0323d3d292a
Basem Elsisy <basemsisy@gmail.com>
2022-09-28 00:04:51 +0200

    feat: install & configure bundle analyzer plugin

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,7 @@
 const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
+const BundleAnalyzerPlugin =
+  require("webpack-bundle-analyzer").BundleAnalyzerPlugin;
 
 module.exports = {
   mode: "production",
@@ -44,9 +46,10 @@ module.exports = {
   },
   plugins: [
     new HtmlWebpackPlugin({
-      title: "Learn Webpack",
+      title: "Joke App",
       filename: "./index.html",
       template: "./src/template.html",
     }),
+    new BundleAnalyzerPlugin(),
   ],
 };
