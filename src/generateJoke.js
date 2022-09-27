efad3b3bef3ddbfa2111c522c18beb70592e744d
Basem Elsisy <basemsisy@gmail.com>
2022-09-27 23:45:00 +0200

    feat: install axios & change generateJoke logic
    load joke from network

--- src/generateJoke.js
+++ src/generateJoke.js
@@ -1,4 +1,13 @@
-function generateJoke() {
-    return "it's a joke from generateJoke()"
+import axios from "axios";
+
+const config = {
+  headers: { Accept: "application/json" },
+};
+
+const jokeElement = document.getElementById("joke");
+
+async function generateJoke() {
+  const joke = await axios.get("https://icanhazdadjoke.com", config);
+  jokeElement.innerHTML = joke.data.joke;
 }
-export default generateJoke
+export default generateJoke;
--- src/index.js
+++ src/index.js
@@ -1,4 +1,12 @@
 import generateJoke from "./generateJoke";
 import "./styles/main.scss";
+import laughImg from "./assets/laughing.svg";
 
-console.log(generateJoke());
+const imgElment = document.getElementById("joke-img");
+const btnElment = document.getElementById("joke-btn");
+
+imgElment.setAttribute("src", laughImg);
+
+btnElment.addEventListener("click", generateJoke);
+
+generateJoke();
